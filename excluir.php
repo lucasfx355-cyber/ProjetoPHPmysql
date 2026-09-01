8bf7cc44ea7b9ea0e0c28f0339b9c398df83ef23
lucasfx355-cyber <lucasfx355@gmail.com>
2026-09-01 11:32:41 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,6 +1,7 @@
 <?php
-id = $_GET['Id:'];
-echo $id;
-
+$id = $_GET['Id'];
+$con = mysqli_connect('localhost', 'root', 'aulaphp');
+mysqli_query ($con, "Delete from aluno where id = '$id' ");
+hearder ("location:buscar.php")
 
 ?>
